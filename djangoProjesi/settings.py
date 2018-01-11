c250295d1284530c4c02d369af9f483ac2647950
Sibel DOĞAN <sibel.dgn03@gmail.com>
2018-01-11 09:59:49 +0300

    Update settings.py

--- djangoProjesi/settings.py
+++ djangoProjesi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k+pmpyf%82umh#d*o-0!e_n+%#=z$brsrhbl5!31en(20(0bo!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
